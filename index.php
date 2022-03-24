3251146232fb28b6e92779cfcb41b4e6e6732f6a
dianachavezr <dianepatrich@gmail.com>
2022-03-24 11:40:11 -0500

    First commit

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $resultado = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 <html lang="es">
 
 <head>
-    <title>Lo tienes CRUDO</title>
+    <title>Lo tienes CRUDO --</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
