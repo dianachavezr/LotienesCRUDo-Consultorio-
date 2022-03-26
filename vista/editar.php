6c7b33853b473f11a359abba89f6e1198402a15f
dianachavezr <dianepatrich@gmail.com>
2022-03-26 17:56:08 -0500

    Styles for main table, header, footer and imgs  added

--- vista/editar.php
+++ vista/editar.php
@@ -1,26 +1,32 @@
 <?php
 require_once("layouts/header.php");
 ?>
-<h1 class="text-center">EDITAR</h1>
+<div class="container-fluid d-flex justify-content-center" style="padding: 20px;">
+
+    <img src="vista/img/OIUH4S01.png" width=100%>
+</div>
+<h1 class="text-center">Editar Cita</h1>
 <?php // aca se traen los datos de la cada id para poder editarlo en vez de un PLACEHOLDER en el input debe haber un value para traer cada dato, tambien se hace un input HIDDEN para ID y para actualizar para así poder traer cada producto por su ID y se llama el método ACTUALIZAR
 ?>
-<form action="" method="get">
-    <?php
-    foreach ($dato as $key => $value) :
-        foreach ($value as $v) :
-    ?>
-            <input type="text" value="<?php echo $v['paciente'] ?>" name="paciente"> <br>
-            <input type="text" value="<?php echo $v['medico'] ?>" name="medico"> <br>
-            <input type="text" value="<?php echo $v['motivo'] ?>" name="motivo"> <br>
-            <input type="date" value="<?php echo $v['fecha'] ?>" name="fecha"> <br>
-            <input type="hidden" value="<?php echo $v['id'] ?>" name="id"> <br>
-            <input type="submit" class="btn" name="btn" value="ACTUALIZAR"> <br>
-            <input type="hidden" name="m" value="actualizar">
-    <?php
+<div class=container>
+    <form action="" method="get">
+        <?php
+        foreach ($dato as $key => $value) :
+            foreach ($value as $v) :
+        ?>
+                <input type="text" value="<?php echo $v['paciente'] ?>" name="paciente"> <br>
+                <input type="text" value="<?php echo $v['medico'] ?>" name="medico"> <br>
+                <input type="text" value="<?php echo $v['motivo'] ?>" name="motivo"> <br>
+                <input type="date" value="<?php echo $v['fecha'] ?>" name="fecha"> <br>
+                <input type="hidden" value="<?php echo $v['id'] ?>" name="id"> <br>
+                <input type="submit" class="btn" name="btn" value="ACTUALIZAR"> <br>
+                <input type="hidden" name="m" value="actualizar">
+        <?php
+            endforeach;
         endforeach;
-    endforeach;
-    ?>
-</form>
+        ?>
+    </form>
+</div>
 
 <?php
 require_once("layouts/footer.php");
--- vista/layouts/header.php
+++ vista/layouts/header.php
@@ -1,15 +1,61 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MVC Básico productos</title>
-    <link rel="stylesheet" href="vista/css/app.css">
+    <title>Lo Tienes CRUDo</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
 </head>
 
 <body>
-    <div class="panel">
-        <h1 class="text-center">SISTEMA DE CITAS</h1>
+
+    <div class='container-fluid' style="height:auto; background-color:#f1ecec; margin:0; padding:0">
+
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php?">Tabla Consulta</a>
+                        </li>
+                        <li class=" nav-item">
+                            <a class="nav-link" href="index.php?m=nuevo">Nueva Cita</a>
+                        </li>
+                        <li class=" nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Información
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Paciente</a></li>
+                                <li><a class="dropdown-item" href="#">Doctor</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <h1 class="text-center" style="padding: 20px 20px 0 20px;">SUSALUD CITAS</h1>
+
+
+
         <!-- contenido-->
